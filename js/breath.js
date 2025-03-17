fdaa94a8b5cf5c736f5ba47bce3dfe1909817290
nnmhd <natchanone@gmail.com>
2025-03-16 20:34:12 -0400

    CSS and OOP added
    Here is CSS and OOP updated.

---
+++ js/breath.js
@@ -0,0 +1,72 @@
+class Breath {
+  constructor(name, inhale, hold, exhale, holdAfterExhale = 0) {
+    this.name = name;
+    this.inhale = inhale;
+    this.hold = hold;
+    this.exhale = exhale;
+    this.holdAfterExhale = holdAfterExhale;
+  }
+
+  getSteps() {
+    return [
+      { phase: "inhale", duration: this.inhale },
+      { phase: "hold", duration: this.hold },
+      { phase: "exhale", duration: this.exhale },
+      { phase: "holdAfterExhale", duration: this.holdAfterExhale },
+    ];
+  }
+
+  startBreathing(callback) {
+    let steps = this.getSteps();
+    let index = 0;
+    this.isBreathing = true;
+
+    const nextStep = () => {
+      if (!this.isBreathing || index >= steps.length) return;
+      let step = steps[index];
+
+      console.log(`${this.name} - ${step.phase} for ${step.duration} seconds`);
+      if (callback) callback(step.phase, step.duration);
+
+      index++;
+      if (index >= steps.length) index = 0;
+      setTimeout(nextStep, step.duration * 1000);
+    };
+    console.log(steps);
+    nextStep();
+  }
+
+  stopBreathing() {
+    this.isBreathing = false;
+    console.log(`${this.name} - Breathing stopped.`);
+  }
+}
+
+class CharBreath extends Breath {
+  constructor(charName, breathReference, move, svgPath) {
+    super(
+      breathReference.name,
+      breathReference.inhale,
+      breathReference.hold,
+      breathReference.exhale,
+      breathReference.holdAfterExhale
+    );
+    this.steps = breathReference.getSteps();
+    this.charName = charName;
+    this.move = move;
+    this.svg = svgPath;
+  }
+
+  moveMent() {
+    console.log(`The ${this.charName} is ${this.move}`);
+  }
+}
+
+class GuidedBreath extends Breath {
+  constructor(inhale, hold, exhale, holdAfterExhale) {
+    super(inhale, hold, exhale, holdAfterExhale);
+  }
+  showGuide() {}
+}
+
+export { Breath, CharBreath, GuidedBreath };
